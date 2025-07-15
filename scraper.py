175235fd50b3ae0a419caf9308d38518ef770cfa
Knox <knoxveyr@gmail.com>
2025-07-15 20:53:31 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,6 +10,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 
