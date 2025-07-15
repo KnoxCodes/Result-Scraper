697623f27cedff9ab3520707cc709ebd322de93f
Knox <knoxveyr@gmail.com>
2025-07-15 20:46:22 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+
+# Install Chrome and Chromedriver
 apt-get update
-apt-get install -y wget unzip
-wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-mv chromedriver /usr/bin/chromedriver
-chmod +x /usr/bin/chromedriver
+apt-get install -y wget unzip curl gnupg
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+
+# Get matching chromedriver
+CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f 1)
+wget -O /tmp/chromedriver.zip "https://chromedriver.storage.googleapis.com/${CHROME_VERSION}.0/chromedriver_linux64.zip"
+unzip /tmp/chromedriver.zip -d /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
